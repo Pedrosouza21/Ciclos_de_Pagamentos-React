e8d97c905d8d433bbcd85aec062e97314dfb50c7
pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-11-09 08:12:47 -0300

    198 - Getting the Qty of Records (Count)

--- src/api/bilingCycle/billingCycleService.js
+++
@@ -1,6 +0,0 @@
-const BillingCycle = require('./billingCycle')
-
-BillingCycle.methods(['get', 'post', 'put', 'delete'])
-BillingCycle.updateOptions({new:true, runValidators: true})
-
-module.exports = BillingCycle
