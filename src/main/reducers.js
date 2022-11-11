954150eb09b27c5ce53da7b05b4839211cddf16b
pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-11-11 08:58:56 -0300

    231 - Creating the First Action and the Reducer

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,9 +1,11 @@
 import  { combineReducers } from 'redux'
 
 import DashBoardReducer from '../dashboard/dashboardReducer'
+import tabReducer from '../commom/tab/tabReducer'
 
 const rootReducer = combineReducers({
-  dashboard: DashBoardReducer 
+  dashboard: DashBoardReducer,
+  tab: tabReducer
 })
 
 export default rootReducer
