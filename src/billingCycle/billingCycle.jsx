274b72fa3853eda9cb5e7cc0218d2e96f2505f04
pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-11-11 08:50:38 -0300

    230 - TabHeader Component

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -1,29 +1,31 @@
-import React, { Component} from 'react '
+import React, { Component } from "react ";
 
-import Content from '../commom/template/content'
-import ContentHeader from '../commom/template/contentHeader'
-import Tabs from '../commom/tab/tabs'
-import TabsContent from '../commom/tab/tabsContent'
-import TabsHeader from '../commom/tab/tabsHeader'
+import Content from "../commom/template/content";
+import ContentHeader from "../commom/template/contentHeader";
+import Tabs from "../commom/tab/tabs";
+import TabsContent from "../commom/tab/tabsContent";
+import TabsHeader from "../commom/tab/tabsHeader";
+import TabHeader from "../commom/tab/tabHeader";
 
 class BillingCycle extends Component {
-    render() {
-        return (
-            <div> 
-                <ContentHeader title= 'Ciclos de Pagamentos' small='Cadastro'/>
-                    <Content>
-                            <Tabs>
-                                <TabsHeader>
-
-                                </TabsHeader>
-                                <TabsContent>
-                                    
-                                </TabsContent>
-                            </Tabs>                  
-                </Content>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <ContentHeader title="Ciclos de Pagamentos" small="Cadastro" />
+        <Content>
+          <Tabs>
+            <TabsHeader>
+              <TabHeader label="Listar" icon="bars" target="tabList" />
+              <TabHeader label="Incluir" icon="plus" target="tabCreate" />
+              <TabHeader label="Alterar" icon="pencil" target="tabUpdate" />
+              <TabHeader label="Excluir" icon="trash-o" target="tabDelete" />
+            </TabsHeader>
+            <TabsContent></TabsContent>
+          </Tabs>
+        </Content>
+      </div>
+    );
+  }
 }
 
-export default BillingCycle
+export default BillingCycle;
