ebbef3f41bc5907eda4291f0fe5267281839384e
pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-11-11 08:23:49 -0300

    229 - Structural Components of Tabs

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -2,14 +2,24 @@ import React, { Component} from 'react '
 
 import Content from '../commom/template/content'
 import ContentHeader from '../commom/template/contentHeader'
+import Tabs from '../commom/tab/tabs'
+import TabsContent from '../commom/tab/tabsContent'
+import TabsHeader from '../commom/tab/tabsHeader'
 
 class BillingCycle extends Component {
     render() {
         return (
             <div> 
                 <ContentHeader title= 'Ciclos de Pagamentos' small='Cadastro'/>
-                <Content>
-                    Ciclos de Pagamentos
+                    <Content>
+                            <Tabs>
+                                <TabsHeader>
+
+                                </TabsHeader>
+                                <TabsContent>
+                                    
+                                </TabsContent>
+                            </Tabs>                  
                 </Content>
             </div>
         )
