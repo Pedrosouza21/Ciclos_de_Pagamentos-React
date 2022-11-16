eaab67b8ce4de2d600a95fe53f129c337812f112
pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-11-16 08:11:15 -0300

    233 - Tab Content Component

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -6,6 +6,7 @@ import Tabs from "../commom/tab/tabs";
 import TabsContent from "../commom/tab/tabsContent";
 import TabsHeader from "../commom/tab/tabsHeader";
 import TabHeader from "../commom/tab/tabHeader";
+import TabContent from "../commom/tab/tabContent";
 
 class BillingCycle extends Component {
   render() {
@@ -20,7 +21,12 @@ class BillingCycle extends Component {
               <TabHeader label="Alterar" icon="pencil" target="tabUpdate" />
               <TabHeader label="Excluir" icon="trash-o" target="tabDelete" />
             </TabsHeader>
-            <TabsContent></TabsContent>
+            <TabsContent>
+              <TabContent id='tabList'><h1>Lista</h1></TabContent>
+              <TabContent id='tabCreate'>Incluir<h1></h1></TabContent>
+              <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+              <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
+            </TabsContent>
           </Tabs>
         </Content>
       </div>
--- src/commom/tab/tabReducer.js
+++ src/commom/tab/tabReducer.js
@@ -1,4 +1,4 @@
-const INITIAL_STATE = { selected: '' }
+const INITIAL_STATE = { selected: 'tabList' }
 
 export default (state = INITIAL_STATE, action )=> {
     switch (action.type) {
